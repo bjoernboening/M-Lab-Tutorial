32cee437bb9b01aeb037113c998a62b0886627ee
Björn Boening <bjoboe@t-online.de>
2017-02-21 10:54:58 +0100

    First tutorial using r with big query

---
+++ Tutorial 01.R
@@ -0,0 +1,8 @@
+# This script may be used to get started with Measurement Lab data on R
+# Author: Bjorn Boening
+# Date created: 21.02.2017
+# Last edited: 21.02.2017
+
+# loading required packages
+library("bigrquery")
+library("dplyr")
